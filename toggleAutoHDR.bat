60e715d0621c389b5f63407f84f46e48490d0b8e
Netrunner <reno5022@gmail.com>
2023-03-01 14:59:09 -0500

    Add files via upload

---
+++ toggleAutoHDR.bat
@@ -0,0 +1,46 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Author: Netrunner
+:: License: GNU General Public License v3.0
+:: Description: Toggle AutoHDR within Windows using parameters
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Usage: toggleAutoHDR.bat /off   |    toggleAutoHDR.bat /on
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+@echo off
+: Find Windows SID
+for /f "delims= " %%a in ('"wmic path win32_useraccount where name='%USERNAME%' get sid"') do (
+   if not "%%a"=="SID" (          
+      set Sid=%%a
+      goto :loop_end
+   )   
+)
+
+:loop_end
+set arg1=%1
+set ValidKey=false
+set AutoHDROn="AutoHDREnable=1;SwapEffectUpgradeEnable=1;"
+set AutoHDROff="AutoHDREnable=0;SwapEffectUpgradeEnable=1;"
+@for /f "tokens=3*" %%i in ('reg query HKEY_USERS\%Sid%\Software\Microsoft\DirectX\UserGpuPreferences /v "DirectXUserGlobalSettings" 2^>Nul') do Set "AutoHDRString=%%i"
+if not defined AutoHDRString goto exit
+
+: Check if AutoHDR is of
+if %AutoHDROff% == "%AutoHDRString%" (
+    set ValidKey=true
+)
+if %AutoHDROn% == "%AutoHDRString%" (
+    set ValidKey=true
+)
+
+if %ValidKey% == true (
+    echo Performing registry swap..
+    if "%arg1%" == "/off" (
+        reg add HKEY_USERS\%Sid%\Software\Microsoft\DirectX\UserGpuPreferences /t REG_SZ /v DirectXUserGlobalSettings /d AutoHDREnable=0;SwapEffectUpgradeEnable=1; /f
+    ) else (
+        reg add HKEY_USERS\%Sid%\Software\Microsoft\DirectX\UserGpuPreferences /t REG_SZ /v DirectXUserGlobalSettings /d AutoHDREnable=1;SwapEffectUpgradeEnable=1; /f
+    )
+) else (
+    echo The registry keys are set to an unexpected value..maybe due to a Windows Update; script stopped automatically.
+    pause
+)
+
+:exit
+exit
