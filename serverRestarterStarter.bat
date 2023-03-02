8fc1aae597fa510e66f6d3d3f07597f7d6aa21b5
Netrunner <reno5022@gmail.com>
2023-03-02 17:23:28 -0500

    Update serverRestarterStarter.bat

--- serverRestarterStarter.bat
+++ serverRestarterStarter.bat
@@ -1,3 +1,8 @@
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Author: Netrunner
+:: License: GNU General Public License v3.0
+:: Description: Restart a server remotely using a smart card and just a .bat!
+::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 @echo off
 echo Starting Server Restarter as SC-%USERNAME%@domain.com..
 runas /smartcard "%USERPROFILE%\Path\To\serverRestarter.bat"
